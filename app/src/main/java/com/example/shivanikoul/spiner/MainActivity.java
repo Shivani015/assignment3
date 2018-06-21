17fa7159012e407e2e1e7605367ff4208ba1d46b
shivani015 <giftykaul@gmail.com>
2018-06-21 22:44:31 +0530

    second commit

--- app/src/main/java/com/example/shivanikoul/spiner/MainActivity.java
+++ app/src/main/java/com/example/shivanikoul/spiner/MainActivity.java
@@ -9,19 +9,19 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    Spinner country,cities;
-    String[] countries ={"India","Germnany","UK","Denmark"};
-    String[] indianCities ={"Mumbai","Pune","Hyderabad","New Delhi"};
-    String[] GermanyCities={"Berlin","Munich","Frankfurt","Leipzig"};
-    String[] UKCities ={"London","Leeds","Bristol","Liverpool"};
-    String[] DenmarkCities ={"Skagen","Odense","Kolding","Aarhus"};
+    Spinner country,states;
+    String[] countries ={"India","Germnany","Japan","Australia"};
+    String[] indianstates ={"Karnataka","Kerela","Rajasthan","Delhi"};
+    String[] Germanystates={"Berlin","Bermin","Lower saxony","saarland"};
+    String[] Japanstates ={"Tokyo","Osaka","Kyoto","Hyogo"};
+    String[] Australiastates ={"western Australa","South Australia","Victoria","Queensland"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         country = findViewById(R.id.country);
-        cities = findViewById(R.id.cities);
+        states = findViewById(R.id.states);
 
         ArrayAdapter<String> countryAdapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, countries);
         country.setAdapter(countryAdapter);
@@ -29,13 +29,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 if(position == 0){
-                    setAdapter(indianCities);
+                    setAdapter(indianstates);
                 }else if (position == 1){
-                    setAdapter(GermanyCities);
+                    setAdapter(Germanystates);
                 }else if (position == 2){
-                    setAdapter(UKCities);
+                    setAdapter(Japanstates);
                 }else if (position == 3){
-                    setAdapter(DenmarkCities);
+                    setAdapter(Australiastates);
                 }
 
             }
@@ -46,10 +46,10 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-    public void setAdapter(String[] citiesArray) {
+    public void setAdapter(String[] statesArray ) {
 
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,android.R.layout.simple_dropdown_item_1line,citiesArray);
-        cities.setAdapter(arrayAdapter);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,android.R.layout.simple_dropdown_item_1line,statesArray);
+        states.setAdapter(arrayAdapter);
 
 
     }
